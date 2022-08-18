77e506967b6d594fc938d2e459f5e851c151dc24
Nykael Cardial <Nykaelccardial@gmail.com>
2022-08-18 16:47:06 -0300

    Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles";
 
 export function Transactions() {
 return(
     <div>
        <Header />
        <Summary />
+
+       <TransactionsContainer>
+        <TransactionsTable>
+            <body>
+                <tr>
+                    <td width="50%">Desenvolvimento de site</td>
+                    <td>
+                        <PriceHighlight variant="income">
+                             R$ 12.000,00
+                        </PriceHighlight>    
+                    </td>
+                    <td>Venda</td>
+                    <td>13/04/2022</td>
+                </tr>
+                <tr>
+                    <td>Hamburguer</td>
+                    <td>
+                        <PriceHighlight variant="outcome">
+                         - R$ 59,00
+                        </PriceHighlight>  
+                    </td>
+                    <td>Alimentação</td>
+                    <td>10/04/2022</td>
+                </tr>
+            </body>
+        </TransactionsTable>
+       </TransactionsContainer>
+
+    
     </div>
 )
 }

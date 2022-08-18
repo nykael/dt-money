e627843b5cc31fb2c5974f1118efb1da3c5e4d1e
Nykael Cardial <Nykaelccardial@gmail.com>
2022-08-18 09:27:07 -0300

    Default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+    white: '#fff',
+
+    'grey-100': '#e1e1e6',
+    'grey-300': '#c4c4cc',
+    'grey-400': '#8d8d99',
+    'grey-500': '#7c7c8a',
+    'grey-600': '#323238',
+    'grey-700': '#29292e',
+    'grey-800': '#202024',
+    'grey-900': '#121214',
+
+    'green-300': '#00b37e',
+    'green-500': '#00875f',
+    'green-700': '#015f43',
+
+    'red-300': '#f75a68',
+    'red-500': '#ab222e',
+    'red-700': '#741921',
+} as const
